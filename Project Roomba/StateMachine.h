389e04eb886a4cf97043aef6b0be2088a61d6078
Jesse A. Hankins <jesse.7.hankins@gmail.com>
2017-10-30 13:12:44 -0500

    Inserted the state machine code into States.h

--- Project Roomba/StateMachine.h
+++ Project Roomba/StateMachine.h
@@ -4,19 +4,19 @@ using namespace std;
 #define STATEMACHINE
 
 class StateMachine {
-	public:
-		enum State { NOT_ASSIGNED,IDLE TRAVERSE, RETREATING};
-		State currentState = NOT_ASSIGNED;
-		State previousState = NOT_ASSIGNED;
-		StateMachine() = default;
-		State GetCurrentState() { return currentState; }
-		State GetPreviousState() { return previousState; }
-		void SetState(State currentState);
-		void SwitchToPreviousState(State currentState);
-		void CheckState();
-		void SetIdle();
-		void SetTraverse();
-		void SetRetreating();
+public:
+	enum State { NOT_ASSIGNED, IDLE TRAVERSE, RETREATING };
+	State currentState = NOT_ASSIGNED;
+	State previousState = NOT_ASSIGNED;
+	StateMachine() = default;
+	State GetCurrentState() { return currentState; }
+	State GetPreviousState() { return previousState; }
+	void SetState(State currentState);
+	void SwitchToPreviousState(State currentState);
+	void CheckState();
+	void SetIdle();
+	void SetTraverse();
+	void SetRetreating();
 };
 
 void StateMachine::SetState(State currentState) {
@@ -35,35 +35,35 @@ void StateMachine::SwitchToPreviousState(State currentState) {
 }
 
 void CheckState() {
-	switch(currentState){
-		case IDLE:
-			if (/*In same room as entity*/) SetRetreating();
-			else if (/*Not in same room and scan complete*/) SetTraverse();
-			break;
-		case TRAVERSE:
-			if (/*In same room as entity*/) SetRetreating();
-			else if (/*Next room has not been scanned*/) SetIdle();
-			break;
-		case RETREATING:
-			if (/*No longer in the same room as entity and traversal not complete*/) SetIdle();
-			break;
-		default:
-			std::cout << "No state found for this state machine object\n";
-			break;
+	switch (currentState) {
+	case IDLE:
+		if (/*In same room as entity*/) SetRetreating();
+		else if (/*Not in same room and scan complete*/) SetTraverse();
+		break;
+	case TRAVERSE:
+		if (/*In same room as entity*/) SetRetreating();
+		else if (/*Next room has not been scanned*/) SetIdle();
+		break;
+	case RETREATING:
+		if (/*No longer in the same room as entity and traversal not complete*/) SetIdle();
+		break;
+	default:
+		std::cout << "No state found for this state machine object\n";
+		break;
 	}
 }
 
-void SetIdle(){//Scan next room for entities before entering
+void SetIdle() {//Scan next room for entities before entering
 	SetState(IDLE);
 
 }
 
-void SetTraverse(){//Finds next room to scan
+void SetTraverse() {//Finds next room to scan
 	SetState(TRAVERSE);
 
 }
 
-void SetRetreating(){//Gets out of dodge to return map
+void SetRetreating() {//Gets out of dodge to return map
 	SetState(RETREATING);
 
 }
--- Project Roomba/States.h
+++ Project Roomba/States.h
@@ -1,7 +1,73 @@
 //this is created as a State Machine, to control the behavior of the robot. 
-#ifndef STATES
-#define STATES
+#ifndef STATEMACHINE
+#define STATEMACHINE
 
-//code here
+#include <iostream>
+using namespace std;
+
+class StateMachine {
+public:
+	enum State { NOT_ASSIGNED, IDLE, TRAVERSE, RETREATING };
+	State currentState = NOT_ASSIGNED;
+	State previousState = NOT_ASSIGNED;
+	StateMachine() = default;
+	State GetCurrentState() { return currentState; }
+	State GetPreviousState() { return previousState; }
+	void SetState(State currentState);
+	void SwitchToPreviousState(State currentState);
+	void CheckState();
+	void SetIdle();
+	void SetTraverse();
+	void SetRetreating();
+};
+
+void StateMachine::SetState(State currentState) {
+	if (this->currentState != NOT_ASSIGNED) {
+		this->previousState = this->currentState;
+	}
+	this->currentState = currentState;
+}
+
+void StateMachine::SwitchToPreviousState(State currentState) {
+	if (this->previousState != NOT_ASSIGNED) {
+		State temp = currentState;
+		this->currentState = this->previousState;
+		this->previousState = temp;
+	}
+}
+
+void StateMachine::CheckState() {
+	switch (currentState) {
+	case IDLE:
+		if (/*In same room as entity*/) SetRetreating();
+		else if (/*Not in same room and scan complete*/) SetTraverse();
+		break;
+	case TRAVERSE:
+		if (/*In same room as entity*/) SetRetreating();
+		else if (/*Next room has not been scanned*/) SetIdle();
+		break;
+	case RETREATING:
+		if (/*No longer in the same room as entity and traversal not complete*/) SetIdle();
+		break;
+	default:
+		std::cout << "No state found for this state machine object\n";
+		break;
+	}
+}
+
+void StateMachine::SetIdle() {//Scan next room for entities before entering
+	SetState(IDLE);
+
+}
+
+void StateMachine::SetTraverse() {//Finds next room to scan
+	SetState(TRAVERSE);
+
+}
+
+void StateMachine::SetRetreating() {//Gets out of dodge to return map
+	SetState(RETREATING);
+
+}
 
 #endif
